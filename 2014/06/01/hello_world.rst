ec01ade1b5c72cde1f933992227d8c29939d95c2
Mario Giustiniani <mariogiustiniani@gmail.com>
2014-06-02 13:46:16 +0200

    first commit

---
+++ 2014/06/01/hello_world.rst
@@ -0,0 +1,9 @@
+Hello World!
+============
+
+
+
+.. author:: default
+.. categories:: none
+.. tags:: rest api nginx
+.. comments::
---
+++ blog/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{about:2},objtypes:{},objnames:{},filenames:["pages/about","2014/06/01/hello_world","master"],titles:["About","Hello World!","Sitemap"],objects:{},titleterms:{world:1,about:0,hello:1,sitemap:2}})
